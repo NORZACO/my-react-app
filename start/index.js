b6a347423999bec4bd9fb49f5cfa5720c40b285f
NORZACO <caspermotive4@gmail.com>
2023-08-02 16:22:35 +0200

    Initail commit

---
+++ start/index.js
@@ -0,0 +1,9 @@
+
+
+
+React.createElement("h1", null, "GEting started with react course"),
+
+ReactDOM.render(
+  // Create a new instance of our "Hello World!" Component
+  document.getElementById('root')
+);
