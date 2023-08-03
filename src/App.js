979c0d8bc19755d904a6fc99f4d8f836ed15477c
NORZACO <caspermotive4@gmail.com>
2023-08-03 02:37:23 +0200

    Destructuring arrays and objects

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { USERS } from './userGenerator';
 
-function App() {
+// Destructuring arrays and objects
+// const [ allUser ] = USERS;
+const [  first_person,  second_person, third_person, fourth_person, fifth_person ] = USERS;
+console.log(first_person, second_person, third_person, fourth_person, fifth_person);
+
+
+
+
+function App({ library }) {
+  const [allUser, setUsers] = useState(USERS);
+  // const [user, setUser] = useState(first);
+
+  // const who = useState()
+  // console.log(who)
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{library} Library</h1>
+        <ul>
+          { allUser.map((user) => {
+            return <li key={user.userId}>
+              <img src={user.avatar} alt={user.username} />
+              <h3>{user.username}</h3>
+              <p>{user.email}</p>
+            </li>;
+          })}
+        </ul>
       </header>
     </div>
   );
